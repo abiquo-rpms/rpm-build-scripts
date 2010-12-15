e92909dabfaf0e00e78f43f20a7eb323d477c602
Sergio Rubio <sergio@rubio.name>
2010-12-15 17:56:00 +0100

    modified:   build-rpms.rb

--- build-rpms.rb
+++ build-rpms.rb
@@ -4,7 +4,8 @@ require 'rpm-robot'
 config = YAML.load_file 'profiles/default.yml'
 
 Dir['build/srpms/*.src.rpm'].each do |srpm|
-  RPMRobot::Log.msg "Mocking srpm #{srpm}... ".ljust(60), false 
+  pkg = srpm.split('/').last
+  RPMRobot::Log.msg "Mocking srpm #{pkg}... ".ljust(60), false 
   res = RPMRobot::Mock.mock_srpm :mock_profile => config[:mock_profile], :srpm => srpm, :result_dir => 'build/rpms'
   if res
     RPMRobot::Log.done
