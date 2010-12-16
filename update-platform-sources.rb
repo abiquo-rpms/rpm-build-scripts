ef18631ec5a340b460ec21e78eb6b4e4dcfd39ce
Sergio Rubio <sergio@rubio.name>
2010-12-16 16:48:22 +0100

    * updated abiquo-ee-1.7 profile
    * added update-platform-sources script

---
+++ update-platform-sources.rb
@@ -0,0 +1,24 @@
+require 'rubygems'
+require 'rpm-robot'
+
+config = YAML.load_file 'profiles/default.yml'
+
+config[:platform_packages].each do |pkg|
+  source_name = pkg.gsub('abiquo-','')
+  RPMRobot::Log.msg "Updating #{source_name} platform sources... ".ljust(60), 
+                    false 
+  cwd = Dir.pwd
+  pkg_dir = File.join('build/repos-ee', pkg)
+  if File.directory?(pkg_dir)
+    Dir.chdir pkg_dir 
+    `rake update_war[#{source_name},#{config[:abiquo_version]}, #{config[:hudson_dir]}]`
+    Dir.chdir cwd
+    if $? == 0
+      RPMRobot::Log.done
+    else
+      RPMRobot::Log.failed
+    end
+  else
+    RPMRobot::Log.failed
+  end
+end
