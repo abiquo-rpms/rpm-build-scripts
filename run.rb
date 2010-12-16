76548fa5a60f0a6b02f729f064d9340467361802
Sergio Rubio <sergio@rubio.name>
2010-12-16 16:24:14 +0100

    * updated build scripts

--- run.rb
+++ run.rb
@@ -15,19 +15,16 @@ FileUtils.mkdir_p 'build/repos-ee'
 
 puts
 puts "Clone/Update common repos".bold
-RestClient.post 'http://localhost:8081/receive', :payload => "Clone/Update common repos" rescue nil
 puts
 require 'clone-common-repos'
 
 puts
 puts "Clone/Update private repos".bold
-RestClient.post 'http://localhost:8081/receive', :payload => "Clone/Update private repos" rescue nil
 puts
 require 'clone-private-repos'
 
 puts
 puts "Create SRPMS".bold
-RestClient.post 'http://localhost:8081/receive', :payload => "Create SRPMS" rescue nil
 puts
 require 'create-srpms'
 
