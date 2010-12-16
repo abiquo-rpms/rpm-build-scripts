9a80fb966bf742613df7efca393c5746f9099a39
Sergio Rubio <sergio@rubio.name>
2010-12-16 16:02:46 +0100

    * updated build scripts and ws-monitor templates

--- run.rb
+++ run.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'fileutils'
 require 'rpm-robot'
 require 'term/ansicolor'
+require 'restclient'
 
 class String
   include Term::ANSIColor
@@ -14,16 +15,19 @@ FileUtils.mkdir_p 'build/repos-ee'
 
 puts
 puts "Clone/Update common repos".bold
+RestClient.post 'http://localhost:8081/receive', :payload => "Clone/Update common repos" rescue nil
 puts
 require 'clone-common-repos'
 
 puts
 puts "Clone/Update private repos".bold
+RestClient.post 'http://localhost:8081/receive', :payload => "Clone/Update private repos" rescue nil
 puts
 require 'clone-private-repos'
 
 puts
 puts "Create SRPMS".bold
+RestClient.post 'http://localhost:8081/receive', :payload => "Create SRPMS" rescue nil
 puts
 require 'create-srpms'
 
