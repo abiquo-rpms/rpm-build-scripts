d1089e85041eaf29bb5064a36aa3bf15dfa27772
Sergio Rubio <sergio@rubio.name>
2010-12-16 13:38:14 +0100

    * updated build scripts

--- run.rb
+++ run.rb
@@ -1,13 +1,33 @@
 require 'rubygems'
 require 'fileutils'
 require 'rpm-robot'
+require 'term/ansicolor'
+
+class String
+  include Term::ANSIColor
+end
 
 FileUtils.mkdir_p 'build/srpms'
 FileUtils.mkdir_p 'build/rpms'
 FileUtils.mkdir_p 'build/repos'
 FileUtils.mkdir_p 'build/repos-ee'
 
+puts
+puts "Clone/Update common repos".bold
+puts
 require 'clone-common-repos'
+
+puts
+puts "Clone/Update private repos".bold
+puts
 require 'clone-private-repos'
+
+puts
+puts "Create SRPMS".bold
+puts
 require 'create-srpms'
+
+puts
+puts "Build SRPMS".bold
+puts
 require 'build-rpms'
