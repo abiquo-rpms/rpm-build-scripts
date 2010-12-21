6b51fa7dd73c97ff0f801ad788ccd61e0e4da545
Sergio Rubio <sergio@rubio.name>
2010-12-21 14:25:44 +0100

    fixes in lib/update-platform-sources.rb

--- lib/update-platform-sources.rb
+++ lib/update-platform-sources.rb
@@ -3,17 +3,16 @@ require 'orpium'
 require 'lib/helpers'
 
 config = YAML.load_file 'profiles/default.yml'
+cwd = Dir.pwd
 
 config[:platform_packages].each do |pkg|
   source_name = pkg.gsub('abiquo-','')
   Orpium::Log.msg "Updating #{source_name} platform sources... ".ljust(60), false 
   begin
-    cwd = Dir.pwd
     pkg_dir = File.join('build/repos-ee', pkg)
     if File.directory?(pkg_dir)
       Dir.chdir pkg_dir 
       repackage_war "http://hudson/#{config[:hudson_dir]}/premium/#{source_name}.war", config[:abiquo_version]
-      Dir.chdir cwd
       Orpium::Log.done
     else
       Orpium::Log.failed
@@ -21,5 +20,7 @@ config[:platform_packages].each do |pkg|
   rescue Exception => e
     Orpium::Log.failed
     $stderr.puts e.msg
+  ensure
+    Dir.chdir cwd
   end
 end
