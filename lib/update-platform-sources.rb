2cf21aa2b06870341e884b4384a3925a718ac52a
Sergio Rubio <sergio@rubio.name>
2010-12-21 14:19:51 +0100

    * better error handling in lib/update-platform-sources.rb

--- lib/update-platform-sources.rb
+++ lib/update-platform-sources.rb
@@ -1,23 +1,24 @@
 require 'rubygems'
 require 'orpium'
+require 'lib/helpers'
 
 config = YAML.load_file 'profiles/default.yml'
 
 config[:platform_packages].each do |pkg|
   source_name = pkg.gsub('abiquo-','')
   Orpium::Log.msg "Updating #{source_name} platform sources... ".ljust(60), false 
-  cwd = Dir.pwd
-  pkg_dir = File.join('build/repos-ee', pkg)
-  if File.directory?(pkg_dir)
-    Dir.chdir pkg_dir 
-    repackage_war "http://hudson/#{config[:hudson_dir]}/premium/#{source_name}.war", config[:abiquo_version]
-    Dir.chdir cwd
-    if $? == 0
+  begin
+    cwd = Dir.pwd
+    pkg_dir = File.join('build/repos-ee', pkg)
+    if File.directory?(pkg_dir)
+      Dir.chdir pkg_dir 
+      repackage_war "http://hudson/#{config[:hudson_dir]}/premium/#{source_name}.war", config[:abiquo_version]
+      Dir.chdir cwd
       Orpium::Log.done
     else
       Orpium::Log.failed
     end
-  else
+  rescue
     Orpium::Log.failed
   end
 end
