3a5135f1f19127a8c560e452e4fe0545fb4e017a
Sergio Rubio <sergio@rubio.name>
2011-01-10 10:31:07 +0100

    fix clone-private-repos

--- lib/clone-private-repos.rb
+++ lib/clone-private-repos.rb
@@ -5,9 +5,5 @@ config = YAML.load_file 'profiles/default.yml'
 config[:abiquo_ee_repos].each do |r|
   Orpium::Log.msg "Fetching repo #{r}... ".ljust(60),false
   res = Orpium::Git.pull_repo('ssh://rpm-git.abiquo.com/git/', r, :dest_dir => 'build/repos-ee')
-  if res
-    Orpium::Log.done
-  else
-    Orpium::Log.failed
-  end
+  Orpium::Log.done
 end
