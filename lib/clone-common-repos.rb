40c0dc925abb3913b55ec16a92c11aecef6efa19
Sergio Rubio <sergio@rubio.name>
2011-01-10 11:30:01 +0100

    * fix clone-common-repos

--- lib/clone-common-repos.rb
+++ lib/clone-common-repos.rb
@@ -8,10 +8,6 @@ repos.delete 'rpm-build-scripts'
 repos.each do |repo|
   Orpium::Log.msg "Fetching repo #{repo}... ".ljust(60), false
   res = Orpium::Github.pull_repo('abiquo-rpms', repo, :dest_dir => 'build/repos')
-  if res
-    Orpium::Log.done
-  else
-    Orpium::Log.failed
-  end
+  Orpium::Log.done
 end
 
