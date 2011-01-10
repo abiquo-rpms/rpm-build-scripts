d8525c3b50cc94047c1895fa42a1dc5f1a508980
Sergio Rubio <sergio@rubio.name>
2011-01-10 18:07:36 +0100

    fix build-rpms

--- lib/build-rpms.rb
+++ lib/build-rpms.rb
@@ -8,11 +8,7 @@ Dir['build/srpms/*.src.rpm'].each do |srpm|
   pkg = srpm.split('/').last
   Orpium::Log.msg "Mocking srpm #{pkg}... ".ljust(60), false 
   res = Orpium::Mock.mock_srpm :mock_profile => config[:mock_profile], :srpm => srpm, :result_dir => 'build/rpms'
-  if res
-    Orpium::Log.done
-  else
-    Orpium::Log.failed
-  end
+  Orpium::Log.done
 end
   
 Dir['build/rpms/*.src.rpm'].each { |f| FileUtils.rm_f f }
